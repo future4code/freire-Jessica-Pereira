13f09a3b15225290160387eaf0ee4a3a0a18407c
Jessica Ferreira Pereira <jessicafpereira16@gmail.com>
2022-05-11 15:41:11 -0300

    exercicios de array de hoje

---
+++ modulo1/strings-arrays/index.js
@@ -0,0 +1,83 @@
+//interpretação de codigo
+
+//exercicio 1
+
+// a. undefined (pois não foi atribuido valor)
+/*let array
+console.log('a. ', array)
+
+// b. null (array foi atribuido null, que também representa falta de valor)
+array = null
+console.log('b. ', array)
+
+// c. 13 (leigth é a quantidades de itens)
+array = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+console.log('c. ', array.length)
+
+// d. 3 (i= 0 é a posição do item dentro da lista)
+let i = 0
+console.log('d. ', array[i])
+
+// d. (11) [3, 19, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+acrescentou depois do indice 0 o número 19
+array[i+1] = 19
+console.log('e. ', array)
+
+// f. 9 identificou o sexto numero depois do indice (i+6) i=0
+const valor = array[i+6]
+console.log('f. ', valor)*/
+
+//exercicio 2
+
+/*const frase = prompt("Digite uma frase")
+
+console.log(frase.toUpperCase().replaceAll("A", "I"), frase.length)*/
+
+/*Qual será o valor impresso no console se a entrada do usuário for:
+ "Subi num ônibus em Marrocos"?*/
+
+ // SUBI NUM ÔNIBUS EM MIRROCOS?
+
+ // toUpperCase troca as letras para maiscula
+ // repalceAll troca os elementos, o primeiro pelo segundo
+
+
+ //exercicio de escrita
+
+ //exercicio 1
+
+    /*let nomeDoUsuario = prompt ("Qual o seu nome?")
+    let emailDoUsuario = prompt ("Digite seu email:")
+    alert (`Seja bem vindo(a) ${nomeDoUsuario}`)*/
+
+//exercicio 2
+
+    /*let comidaUsuario = prompt ("Qual a sua comida favorita?")
+    let comidasFavoritas = ["massa", "batata frita", comidaUsuario , "xis burguer", "lasanha"]
+    
+
+    console.log (comidasFavoritas)
+
+    console.log ("Essas são minhas comidas favoritas")
+    console.log (comidasFavoritas[0])
+    console.log (comidasFavoritas[1])
+    console.log (comidasFavoritas[2])
+    console.log (comidasFavoritas[3])
+    console.log (comidasFavoritas[4])*/
+
+
+    //exercicio 3
+
+    /*let listaDeTarefas = prompt ("Diga a primeira tarefa que realiza no dia:")
+    let listaDeTarefas2 = prompt ("Diga a segunda tarefa que realiza no dia:")
+    let listaDeTarefas3 = prompt ("Diga a terceira tarefa que realiza no dia:")
+    let resultado = [listaDeTarefas, listaDeTarefas2, listaDeTarefas3]
+    
+    console.log (resultado)
+   
+    let tarefaConcluida = prompt ("Qual dessas tarefas você já fez? 0 (primeira), 1 (segunda) ou 2 (terceira)?")
+    
+    resultado.splice (tarefaConcluida, 1)
+    
+    console.log (resultado)*/
+    
